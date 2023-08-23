fee3a7ab0c5a137025e683ca1dc37fa9fc1071ae
oliverhees <oliverhees@gmail.com>
2023-08-23 21:23:53 +0200

    add Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,9 @@ function Home() {
         <Navbar/>
         <Body>
           <Hero/>
-          <Skills/>
+          <Wrapper>
+            <Skills/>
+          </Wrapper>
         </Body>
       </ThemeProvider>
     )
@@ -20,9 +22,25 @@ function Home() {
 
 export default Home;
 
+/* Style Components */
 const Body = styled.div`
 background-color: ${({theme}) => theme.bg};
 width: 100%;
 height: 100%;
 overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+background: linear-gradient(
+  38.73deg,
+  rgba(204,0,187,0.15) 0%,
+  rgba(201,32,184,0) 50%
+),
+linear-gradient(
+  141.27deg,
+  rgba(0,70,209,0) 50%,
+  rgba(0,70,209,0.15) 100%
+);
+width: 100%;
+clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,171 @@
+import { styled } from "styled-components";
+import Link from "next/link";
+import { SiCodechef } from "react-icons/si";
+
 const Navbar = () => {
-    return(<div>Navbar</div>); 
-}
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo to="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20",
+              cursor: "pointer",
+            }}
+          >
+            <SiCodechef size="3rem" />
+          </a>
+          <Span>
+            Oliver Hees
+            <SubLine>Full-Stack Developer</SubLine>
+          </Span>
+        </NavLogo>
+
+        <MobileIcon></MobileIcon>
+        <NavItems>
+          <NavLink href="#about">Home</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GithubButton>Github Profile</GithubButton>
+        </ButtonContainer>
+      </NavContainer>
+    </Nav>
+  );
+};
 
 export default Navbar;
+
+/* Styled Components */
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.card_light};
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media screen and (max-widht: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
+`;
+
+const NavLogo = styled.div`
+  width: 80%;
+  padding: 0 6px;
+  display: flex;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  align-items: center;
+  @media screen and (max-width: 640px) {
+    padding: 0 0px;
+  }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 50%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  height: 100%;
+  padding: 0 6px;
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const GithubButton = styled.button`
+  background-color: transparent;
+  color: ${({ theme }) => theme.primary};
+  border: 1.8px solid ${({ theme }) => theme.primary};
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 20px;
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+  height: 70%;
+  &:hover {
+    background-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.white};
+  }
+
+  @media screen and (max-width: 640px) {
+    font-size: 0.8rem;
+  }
+`;
+
+const Span = styled.span`
+  padding: 0 4px;
+  font-weight: bold;
+  font-size: 18px;
+  color: ${({ theme }) => theme.white};
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-items: start;
+`;
+
+const SubLine = styled.span`
+  font-weight: normal;
+  font-size: 12px;
+  color: ${({ theme }) => theme.white};
+`;
