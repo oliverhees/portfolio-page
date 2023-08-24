9b49e59b3acd29dcd2de5cfa9ca9bb90319334bc
oliverhees <oliverhees@gmail.com>
2023-08-24 13:01:18 +0200

    add hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,253 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+import { Bio } from "@/data/const";
+import Typewriter from "typewriter-effect";
+import HeroImg from "@/images/HeroImage.png";
+import Image from "next/image";
+import HeroBgAnimation from "../HeroBgAnimation";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeft>
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="_blank">
+              Check Resume
+            </ResumeButton>
+          </HeroLeft>
+          <HeroRight>
+            <Image src={HeroImg} alt="Hero Image" />
+          </HeroRight>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
+
+const HeroContainer = styled.div`
+  background-color: ${({ theme }) => {
+    theme.card_light;
+  }};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+
+  @media screen and (max-width: 960px) {
+    padding: 66px 16px;
+  }
+
+  @media screen and (max-width: 640px) {
+    padding: 32px 16px;
+  }
+
+  z-index: 1;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 50%;
+  right: 0;
+  bottom: 0;
+  left: 50%;
+  overflow: hidden;
+  width: 85%;
+  height: 100%;
+  padding: 0 30px;
+  transform: translateX(-50%) translateY(-50%);
+
+  @media screen and (max-width: 960px) {
+    padding: 0 0px;
+    justify-content: center;
+  }
+`;
+
+const HeroInnerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeft = styled.div`
+  width: 100%;
+  order: 1;
+
+  @media screen and (max-width: 960px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media screen and (max-width: 640px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+const HeroRight = styled.div`
+  width: 100%;
+  order: 2;
+  display: flex;
+  gap: 12px;
+  justify-content: end;
+
+  img {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    border-radius: 50%;
+    max-height: 400px;
+    max-width: 400px;
+    object-fit: cover;
+    object-position: center;
+    border: 2px solid ${({ theme }) => theme.primary};
+
+    @media screen and (max-width: 960px) {
+      max-height: 400px;
+      max-width: 400px;
+    }
+
+    @media screen and (max-width: 640px) {
+      max-height: 280px;
+      max-width: 280px;
+    }
+  }
+
+  @media screen and (max-width: 960px) {
+    order: 1;
+    margin-bottom: 30px;
+  }
+
+  @media screen and (max-width: 640px) {
+    order: 1;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 640px) {
+    font-size: 40px;
+    line-height: 48px;
+    margin-bottom: 8px;
+  }
+`;
+
+const TextLoop = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+  gap: 12px;
+  display: flex;
+
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 640px) {
+    font-size: 22px;
+    line-height: 48px;
+    margin-bottom: 16px;
+  }
+`;
+
+const Span = styled.span`
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+`;
+
+const SubTitle = styled.p`
+  font-size: 20px;
+  color: ${({ theme }) => theme.text_primary + 95};
+  line-height: 32px;
+  margin-bottom: 42px;
+
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 640px) {
+    font-size: 16px;
+    line-height: 32px;
+  }
+`;
+
+const ResumeButton = styled.a`
+  appearance: button;
+  text-decoration: none;
+  width: 95%;
+  max-width: 300px;
+  text-align: center;
+  padding: 16px 0;
+  color: ${({ theme }) => theme.white};
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  transition: all 0.2s ease-in-out;
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(271, 100%, 50%, 0.5) 100%
+  );
+  box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+  &:hover {
+    transform: scale(1.05);
+    transition: all 0.4s ease-in-out;
+    box-shadow: 20px 20px 60px #1f2634;
+    filter: brightness(1);
+  }
+
+  @media (max-width: 640px) {
+    padding: 12px 0;
+    font-size: 18px;
+  }
+`;
--- src/data/const.js
+++ src/data/const.js
@@ -5,6 +5,8 @@ export const Bio = {
         "WordPress Developer",
         "Low-Code Developer",
         "Programmer",
+        "Online Marketer",
+        "YouTuber",
     ],
     description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/oliverhees",
